53c3d198c13a12a177b646dcdac5620667b3d3d0
Ігор <shelest.igor02@gmail.com>
2026-03-30 01:06:51 +0300

    створив пайп для обрізки тексту

--- src/app/shared/components/card/card.ts
+++ src/app/shared/components/card/card.ts
@@ -1,20 +1,27 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { DatePipe, CurrencyPipe, UpperCasePipe, CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router'; // Додано для навігації
+import { RouterModule } from '@angular/router';
 import { Product, ProductCategory } from '../../models/product';
+import { TruncatePipe } from '../../pipes/truncate-pipe';
+
 
 @Component({
   selector: 'fashion-card',
   standalone: true,
-  imports: [DatePipe, CurrencyPipe, UpperCasePipe, CommonModule, RouterModule], 
+  imports: [
+    DatePipe, 
+    CurrencyPipe, 
+    UpperCasePipe, 
+    CommonModule, 
+    RouterModule, 
+    TruncatePipe 
+  ],
   templateUrl: './card.html',
   styleUrl: './card.css'
 })
 export class CardComponent {
   @Input({ required: true }) item!: Product;
-
   @Output() addToCart = new EventEmitter<number>();
-
   protected readonly ProductCategory = ProductCategory;
 
   onBuyClick(): void {
