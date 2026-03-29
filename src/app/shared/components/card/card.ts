18f4b1666fa2a47460ff52005566eb42b0df49d4
Ігор <shelest.igor02@gmail.com>
2026-03-30 00:46:32 +0300

    реалізувано навігацію зі списку продуктів до деталей продукту за допомогою routerLink

--- src/app/shared/components/card/card.ts
+++ src/app/shared/components/card/card.ts
@@ -1,11 +1,12 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core'; // Додали імпорти
-import { DatePipe, CurrencyPipe, UpperCasePipe } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { DatePipe, CurrencyPipe, UpperCasePipe, CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // Додано для навігації
 import { Product, ProductCategory } from '../../models/product';
 
 @Component({
   selector: 'fashion-card',
   standalone: true,
-  imports: [DatePipe, CurrencyPipe, UpperCasePipe],
+  imports: [DatePipe, CurrencyPipe, UpperCasePipe, CommonModule, RouterModule], 
   templateUrl: './card.html',
   styleUrl: './card.css'
 })
