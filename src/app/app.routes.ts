e9b07822aad34fbc4b550e6178bb9f558f31766d
Ігор <shelest.igor02@gmail.com>
2026-03-30 13:45:46 +0300

    додано маршрут входу та реактивну перевірку форми

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { ListComponent } from './features/products/list/list';
 import { ProductDetailsComponent } from './features/product-details/product-details';
 import { NotFoundComponent } from './core/pages/not-found/not-found';
 import { ProductFormComponent } from './features/product-form/product-form';
+import { Login } from './core/components/login/login';
+import { Register } from './core/components/register/register';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ListComponent },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
   { path: 'product/new', component: ProductFormComponent },
   { path: 'product/:id', component: ProductDetailsComponent },
   { path: '**', component: NotFoundComponent }
