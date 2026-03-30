7666e686b9aa68f35fed59279e4f7a8cb9ba8beb
Ігор <shelest.igor02@gmail.com>
2026-03-30 13:52:51 +0300

    реалізував функціонального охоронця маршруту

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,20 @@ import { NotFoundComponent } from './core/pages/not-found/not-found';
 import { ProductFormComponent } from './features/product-form/product-form';
 import { Login } from './core/components/login/login';
 import { Register } from './core/components/register/register';
+import { authGuard } from './core/guards/auth-guard'; 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ListComponent },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
-  { path: 'product/new', component: ProductFormComponent },
+  
+  { 
+    path: 'product/new', 
+    component: ProductFormComponent, 
+    canActivate: [authGuard] 
+  },
+  
   { path: 'product/:id', component: ProductDetailsComponent },
   { path: '**', component: NotFoundComponent }
 ];
