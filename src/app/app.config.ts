c79d7628a9bf5dac58066d86e0278f0c9c3b4e7b
Ігор <shelest.igor02@gmail.com>
2026-03-30 12:15:48 +0300

    налаштував http-клієнт та інтерцептор API

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
-
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
+import { apiInterceptor } from './core/interceptors/api-interceptor'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withComponentInputBinding())
+    provideRouter(routes),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+    }),
+    // Підключаємо HTTP клієнт та наш інтерцептор
+    provideHttpClient(
+      withInterceptors([apiInterceptor])
+    )
   ]
 };
