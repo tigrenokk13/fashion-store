23fb83ed2071a94d5ea53c51a0e45aec67170075
Ігор <shelest.igor02@gmail.com>
2026-03-30 14:28:35 +0300

    реалізував реактивний інтерфейс користувача з відстеженням статусу автентифікації

--- src/app/core/header/header.ts
+++ src/app/core/header/header.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../services/auth';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { AppInfo } from '../../shared/models/appInfo';
 
 @Component({
   selector: 'fashion-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class HeaderComponent {
+  private authService = inject(AuthService);
+
   public headerConfig: AppInfo = {
     title: 'Модний одяг',
     year: 2026 
   };
+
+  isLoggedIn$ = this.authService.isAuth$;
+
+  logout() {
+    this.authService.logout();
+  }
 }
